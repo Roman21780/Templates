4ef060f3cf57261dcd8d2e3de6b10f98984bcc37
Roman <Sapunovrm@bk.ru>
2025-03-11 18:01:06 +0500

    Iterator

---
+++ src/Iterator/Randoms.java
@@ -0,0 +1,33 @@
+package Iterator;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    private final int min;
+    private final int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        this.random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RandomIterator();
+    }
+
+    private class RandomIterator implements Iterator<Integer> {
+        @Override
+        public boolean hasNext() {
+            return true; // Итератор бесконечный, всегда возвращает true
+        }
+
+        @Override
+        public Integer next() {
+            return random.nextInt(max - min + 1) + min;
+        }
+    }
+}
